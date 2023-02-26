086e0efee7c2208137c649c57b336be19a494921
usuario <usuario@LAPTOP-N08BQ166>
2023-02-26 09:57:44 -0600

    Agregar archivos de proyecto.

---
+++ informacion/informacion/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace informacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            string nombre = EntryParaNombre.Text;
+            string apellido = EntryParaApellido.Text;
+            string edad = EntryParaeedad.text;
+            string correo = EntryParacorreo.text;
+
+            await Application.Current.MainPage.Navigation.PushAsync(new Pagina2view(nombre, apellido,edad,correo));
+        }
+
+    }
+}
